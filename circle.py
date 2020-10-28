17d0c6acdccb69f7c562bef6868f0d7b884a9a08
elv-wayne <wayne.tran@eluv.io>
2020-10-28 16:04:39 -0400

    Create circle module

---
+++ circle.py
@@ -0,0 +1,9 @@
+import math
+
+
+def circumference(r):
+    return 2*math.pi*r
+
+
+def area(r):
+    return math.pi*r**2
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+import circle
+
+
+def main():
+    r = 3
+    print(circle.circumference(r))
+    print(circle.area(r))
+
+
+if __name__ == "__main__":
+    main()
